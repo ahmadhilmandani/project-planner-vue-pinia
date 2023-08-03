b35cb6d0921b09cc4ab0fde9483716c0d8ac6a26
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-08-03 14:06:12 +0700

    FIRST COMMIT: PROJECT PREP

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="bg-zinc-950 flex justify-center items-center w-full h-screen">
+    <h1 class="text-3xl text-white font-bold">Hello, World!</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
