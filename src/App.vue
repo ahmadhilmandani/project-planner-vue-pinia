3ddf913fd4a91a1878c0eace72ea2ea18e1b1c74
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-08-05 16:14:11 +0700

    UPDATE card project components

--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,66 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import CardProject from './components/CardProject.vue'
+import { ref } from 'vue';
+
+const projectsList = ref(
+  [
+    {
+      id: "1",
+      title: "Belajar Pinia",
+      deadline: "05 Desember 2023",
+      deadlineLeft: "11 Hari",
+      body: "When used with Vue <2.7, you need to install the @vue/composition-api plugin. You can learn how to do that here. Once this is done, you can proceed with the below.",
+      thingsToDo: [
+        "Vue JS basic",
+        "Kuota Internet",
+        "Kopi",
+      ],
+      isDone: false
+    },
+    {
+      id: "2",
+      title: "Belajar Supabase",
+      deadline: "27 Januari 2024",
+      deadlineLeft: "42 Hari",
+      body: "complicated classes you want to add to your project that you do still like Use the components layer Vuelidate is considered model-based because the validation rules are defined next to your data, and the validation tree structure matches the data model structure.",
+      thingsToDo: [
+        "Vue JS basic",
+        "Pinia",
+        "Kuota Internet",
+        "Kopi",
+      ],
+      isDone: true
+    },
+  ]
+)
+
 </script>
 
 <template>
   <div class="bg-neutral-950 flex flex-col justify-center items-center w-full min-h-screen gap-10 p-20">
     <h1 class="text-6xl text-white font-bold">Simple Project Planner</h1>
 
-    <!-- tampilan card bila BELUM selesai projectnya -->
-    <CardProject>
-      <template #title>
-        Belajar Vue JS 3
-      </template>
-      <template #deadline>
-        05 Mei 2023
-      </template>
-      <template #deadlineLeft>
-        11 Hari
-      </template>
-      <template #body>
-        {{ ` When used with Vue <2.7, you need to install the @vue/composition-api plugin. You can learn how to do that
-          here. Once this is done, you can proceed with the below.`}} </template>
-          <template #prepNeeded>
-            <div class="text-white text-sm mt-6 px-5 flex justify-between items-center">
-              <div>
-                <div>Preperation Needed: </div>
-                <ul class="list-disc">
-                  <li>Satu satu satu</li>
-                  <li>Dua dua dua</li>
-                  <li>Tiga tiga tiga</li>
-                </ul>
-              </div>
-              <div class="w-1/3">
-                <h1 class="w-full p-2 text-center text-lg rounded-full bg-rose-500 border-[0.8px] border-rose-300">
-                  Belum Selesai
-                </h1>
-                <div class="flex mt-2 items-center justify-center gap-2">
-                  <h1
-                    class="flex-1 p-2 text-center text-sm rounded-full bg-emerald-500 border-[0.8px] border-emerald-300 hover:bg-opacity-30 cursor-pointer">
-                    Selesaikan
-                  </h1>
-                  <div
-                    class="border-[0.8px] border-neutral-700 flex-1 p-2 rounded-full hover:bg-opacity-30 cursor-pointer hover:bg-neutral-700">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash mx-auto" width="16"
-                      height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                      stroke-linecap="round" stroke-linejoin="round">
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                      <path d="M4 7l16 0"></path>
-                      <path d="M10 11l0 6"></path>
-                      <path d="M14 11l0 6"></path>
-                      <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
-                      <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
-                    </svg>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </template>
-    </CardProject>
-
-    <!-- tampilan card bila SUDAH selesai projectnya -->
-    <CardProject>
-      <template #title>
-        Belajar Pinia
-      </template>
-      <template #deadline>
-        25 Mei 2023
-      </template>
-      <template #deadlineLeft>
-        31 Hari
-      </template>
-      <template #body>
-        complicated classes you want to add to your project that you do still like Use the components layer
-        Vuelidate is considered model-based because the validation rules are defined next to your data, and the
-        validation tree structure matches the data model structure.
-      </template>
-      <template #prepNeeded>
-        <div class="text-white text-sm mt-6 px-5 flex justify-between items-center">
-          <div>
-            <div>Preperation Needed: </div>
-            <ul class="list-disc">
-              <li>Satu satu satu</li>
-              <li>Dua dua dua</li>
-              <li>Tiga tiga tiga</li>
-            </ul>
-          </div>
-          <div class="w-1/3">
-            <h1 class="w-full p-2 text-center text-lg rounded-full bg-emerald-500 border-[0.8px] border-emerald-300">
-              Selesai
-            </h1>
-            <div class="flex mt-2 items-center justify-center gap-2">
-              <div
-                class="border-[0.8px] border-neutral-700 flex-1 p-2 rounded-full hover:bg-opacity-30 cursor-pointer hover:bg-neutral-700">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash mx-auto" width="16"
-                  height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                  stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                  <path d="M4 7l16 0"></path>
-                  <path d="M10 11l0 6"></path>
-                  <path d="M14 11l0 6"></path>
-                  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
-                  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
-                </svg>
-              </div>
-            </div>
-          </div>
-        </div>
-      </template>
-    </CardProject>
+    <div v-for="project in projectsList">
+      <CardProject :isDone="project.isDone">
+        <template #title>
+          {{ project.title }}
+        </template>
+        <template #deadline>
+          {{ project.deadline }}
+        </template>
+        <template #deadlineLeft>
+          {{ project.deadlineLeft }}
+        </template>
+        <template #body>
+          {{ project.body }}
+        </template>
+        <template #thingsToDo>
+          <ul class="list-disc" v-for="thingTodo in project.thingsToDo">
+            <li>{{ thingTodo }}</li>
+          </ul>
+        </template>
+      </CardProject>
+    </div>
   </div>
 </template>
 
--- src/components/CardProject.vue
+++ src/components/CardProject.vue
@@ -1,6 +1,14 @@
+<script setup>
+defineProps({
+  isDone: Boolean
+})
+
+</script>
+
+
 <template>
-  <div
-    class="bg-neutral-900 border-[0.8px] border-neutral-800 w-[560px] rounded-tr-3xl px-4 py-8">
+  <div class="bg-neutral-900 border-[0.8px] border-neutral-800 w-[560px] rounded-tr-3xl px-4 py-8">
+    
     <div class="flex gap-8 items-center">
       <h1 class="text-2xl text-white bg-neutral-800 px-5 py-2 rounded-full w-fit">
         <slot name="title"></slot>
@@ -24,9 +32,52 @@
         <slot name="deadlineLeft"></slot>
       </p>
     </div>
-    <p class="text-white text-sm mt-5 p-5 border-t-[0.8px] border-b-[0.8px] border-neutral-700">
-      <slot name="body"></slot>
-    </p>
-    <slot name="prepNeeded"></slot>
+
+    <div class="text-white text-sm mt-5 p-5 border-t-[0.8px] border-b-[0.8px] border-neutral-700">
+      <div class="text-sm text-neutral-700 mb-2">
+        Deskripsi :
+      </div>
+      <p>
+        <slot name="body"></slot>
+      </p>
+    </div>
+
+    <div class="text-white text-sm mt-6 px-5 flex justify-between items-center">
+      <div>
+        <div>Hal yang harus dilakukan </div>
+        <slot name="thingsToDo">
+        </slot>
+      </div>
+      <div class="w-1/3">
+        <h1 v-if="isDone"
+          class="flex-1 p-2 text-center text-sm rounded-full bg-emerald-500 border-[0.8px] border-emerald-300">
+          Selesai
+        </h1>
+        <h1 v-else class="w-full p-2 text-center text-lg rounded-full bg-rose-500 border-[0.8px] border-rose-300"
+          @click="changeDate">
+          Belum Selesai
+        </h1>
+        <div class="flex mt-2 items-center justify-center gap-2">
+          <h1 v-if="isDone === false"
+            class="flex-1 p-2 text-center text-sm rounded-full bg-emerald-500 border-[0.8px] border-emerald-300 hover:bg-opacity-30 cursor-pointer">
+            Selesaikan
+          </h1>
+          <div
+            class="border-[0.8px] border-neutral-700 flex-1 p-2 rounded-full hover:bg-opacity-30 cursor-pointer hover:bg-neutral-700">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash mx-auto" width="16"
+              height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+              stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M4 7l16 0"></path>
+              <path d="M10 11l0 6"></path>
+              <path d="M14 11l0 6"></path>
+              <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+              <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
