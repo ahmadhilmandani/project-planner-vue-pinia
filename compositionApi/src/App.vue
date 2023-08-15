699f7da5838ee3ff47431163e31b949e516a2a84
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-08-15 22:17:59 +0700

    PROJECT PREP build the same website but using composition Api and pinia

---
+++ compositionApi/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="bg-neutral-950 w-full min-h-screen text-white">
+    <nav class="bg-neutral-900 w-full h-16 flex justify-center items-center gap-10">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
+</template>
+
