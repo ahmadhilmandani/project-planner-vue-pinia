b02dc80b7f72d7769b9f203f871a4a9043cfb187
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-08-15 23:38:44 +0700

    DELETE unused file and code

--- optionsApi/src/App.vue
+++ optionsApi/src/App.vue
@@ -18,37 +18,6 @@ export default {
     toogleIsDone(index) {
       this.projectsList[index].isDone = true
       console.log(this.projectsList[index].isDone)
-    },
-    test() {
-      localStorage.setItem("projectsList", JSON.stringify([
-        {
-          id: "1",
-          title: "Belajar Pinia",
-          deadline: "05 Desember 2023",
-          deadlineLeft: "11 Hari",
-          body: "When used with Vue <2.7, you need to install the @vue/composition-api plugin. You can learn how to do that here. Once this is done, you can proceed with the below.",
-          thingsToDo: [
-            "Vue JS basic",
-            "Kuota Internet",
-            "Kopi",
-          ],
-          isDone: false
-        },
-        {
-          id: "2",
-          title: "Belajar Supabase",
-          deadline: "27 Januari 2024",
-          deadlineLeft: "42 Hari",
-          body: "complicated classes you want to add to your project that you do still like Use the components layer Vuelidate is considered model-based because the validation rules are defined next to your data, and the validation tree structure matches the data model structure.",
-          thingsToDo: [
-            "Vue JS basic",
-            "Pinia",
-            "Kuota Internet",
-            "Kopi",
-          ],
-          isDone: false
-        },
-      ]))
     }
   }
 }
@@ -57,7 +26,6 @@ export default {
 
 <template>
   <div>
-    <!-- <div class="fixed top-0 bottom-0 left-0 right-0 bg-blue-500 z-[99]" @click="test"></div> -->
     <SidebarNav />
     <TopbarNav />
     <router-view />
