474687d0e122bbc8f2df4a57c2ebab975466ead8
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-08-16 16:49:36 +0700

    ADD TopNavbar component

--- compositionApi/src/App.vue
+++ compositionApi/src/App.vue
@@ -1,14 +1,47 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, ref, watchEffect } from 'vue'
+import { IconClick } from '@tabler/icons-vue'
+import TopNavbar from './components/TopNavbar.vue'
+
+export default {
+  setup() {
+    console.log('commposition')
+
+    const test = ref(null)
+    const searchInput = ref('')
+    const arraysTest = ref(['mario', 'yoshi', 'toad', 'bowser', 'ahmad', 'hilman', 'dani'])
+
+    let name = 'mario'
+
+    const handleClick = () => {
+      console.log(test)
+      console.log(test.value)
+      test.value.textContent = test + "   " + test.value
+      test.value.classList.add('bg-rose-500')
+    }
+
+    const computedArraysTest = computed(() => {
+      console.log('jalan')
+      return arraysTest.value.filter((arrayTest) => {
+        return arrayTest.includes(searchInput.value)
+      })
+    })
+
+    watchEffect(() => {
+      console.log("testinggg")
+    }, searchInput)
+
+
+    return { name, test, handleClick, searchInput, arraysTest, computedArraysTest }
+  },
+  components: { IconClick, TopNavbar }
+}
 </script>
 
 <template>
-  <div class="bg-neutral-950 w-full min-h-screen text-white">
-    <nav class="bg-neutral-900 w-full h-16 flex justify-center items-center gap-10">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
+  <div class="w-full min-h-screen bg-neutral-950">
+    <TopNavbar />
     <RouterView />
   </div>
 </template>
