d1e4f91145551417f25fd15edfc86bdfe482b425
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-08-18 22:05:50 +0700

    UPDATE routing

--- compositionApi/src/components/TopNavbar.vue
+++ compositionApi/src/components/TopNavbar.vue
@@ -12,8 +12,8 @@ export default {
   <nav
     class="w-full h-14 px-16 flex justify-end items-center gap-16 fixed top-0 left-0 right-0 z-[2000] bg-neutral-900 border-[0.8px] border-t-0 border-neutral-800">
     <RouterLink class="hover:opacity-60 text-xs" :to="{ name: 'all' }">Semua</RouterLink>
-    <RouterLink class="hover:opacity-60 text-xs" to="/on-progress">Berjalan</RouterLink>
-    <RouterLink class="hover:opacity-60 text-xs" to="/on-progress">Selesai</RouterLink>
+    <RouterLink class="hover:opacity-60 text-xs" :to="{ name: 'OnProgress' }">Berjalan</RouterLink>
+    <RouterLink class="hover:opacity-60 text-xs" :to="{ name: 'done' }">Selesai</RouterLink>
     <a class="hover:opacity-60 text-xs rounded-full px-3 py-1 bg-neutral-950 border-[0.8px] border-neutral-700"
       href="https://ahmadhilmandani.netlify.com/" target="_blank">Tentang Developer</a>
   </nav>
