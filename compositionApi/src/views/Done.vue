a9e32abab4a330d81c57e4c7e0be67fff2553be3
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-08-18 22:09:33 +0700

    ADD done vies

---
+++ compositionApi/src/views/Done.vue
@@ -0,0 +1,51 @@
+<script>
+import { ref, computed } from 'vue';
+import CardProject from '../components/CardProject.vue'
+import getProjects from '../composables/getProjects';
+import toogleIsDone from '../composables/toogleIsDone'
+import deleteProject from '../composables/deleteProject'
+
+export default {
+  setup() {
+    const projects = ref(getProjects())
+
+    const filteredProjects = computed(() => {
+      return projects.value.filter((project) => project.isDone === true)
+    })
+
+    return { projects, toogleIsDone, deleteProject, filteredProjects }
+  },
+  components: { CardProject }
+}
+</script>
+
+<template>
+  <div class="bg-neutral-950 flex flex-col justify-center items-center w-full min-h-screen gap-10 px-10 py-24">
+    <h1 class="text-6xl text-white font-bold">Simple Project Planner</h1>
+    <div v-for="(project, index) in filteredProjects">
+      <CardProject :isDone="project.isDone" @toogleIsDone="() => { toogleIsDone(projects, project.id) }"
+        @deleteProject="() => { deleteProject(projects, project.id) }">
+        <template #title>
+          {{ project.title }}
+        </template>
+        <template #deadline>
+          {{ project.deadline }}
+        </template>
+        <template #deadlineLeft>
+          {{ project.deadlineLeft }}
+        </template>
+        <template #body>
+          {{ project.body }}
+        </template>
+        <template #thingsToDo>
+          <ol v-if="project.stepToDo.length != 0" v-for="(thingTodo, index) in project.stepToDo">
+            <li>{{ (++index) + ". " + thingTodo }}</li>
+          </ol>
+          <ol v-else>
+            <li>-</li>
+          </ol>
+        </template>
+      </CardProject>
+    </div>
+  </div>
+</template>
