e8063d02eb09de63cb2f7937766054d8263ecb14
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-08-18 10:58:15 +0700

    UPDATE deleteProject function is composables now

---
+++ compositionApi/src/composables/deleteProject.js
@@ -0,0 +1,10 @@
+export default function deleteProject(projects, projectId) {
+
+  const index = projects.findIndex(project => project.id === projectId);
+  if (index !== -1) {
+    projects.splice(index, 1);
+  }
+
+  localStorage.setItem("projects", JSON.stringify(projects))
+
+}
