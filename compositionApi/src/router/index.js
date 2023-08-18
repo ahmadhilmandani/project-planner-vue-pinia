86aae596b6c7aed781214dfa14c49c37419a110d
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-08-18 22:07:34 +0700

    UPDATE routing

--- compositionApi/src/router/index.js
+++ compositionApi/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AllProject from '../views/AllProject.vue'
+import Done from '../views/Done.vue'
+import OnProgress from '../views/OnProgress.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'all',
       component: AllProject
+    },
+    {
+      path: '/done',
+      name: 'done',
+      component: Done
+    },
+    {
+      path: '/on-progress',
+      name: 'OnProgress',
+      component: OnProgress
     }
   ]
 })
