c77267c5450956fdcdb2f0aeed63ebd78727c176
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-08-14 13:58:38 +0700

    Add logout functionality

--- src/components/SidebarAddForm.vue
+++ src/components/SidebarAddForm.vue
@@ -1,15 +1,39 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
 import { IconBriefcase2, IconChevronDown, IconUserCircle, IconLogout, IconPointFilled } from '@tabler/icons-vue'
+import axios from 'axios';
+import { toast } from 'vue3-toastify';
+import { useRouter } from 'vue-router';
 
 const isProjectsOpen = ref(true)
+const router = useRouter()
+
+function handleLogout() {
+  axios.post('http://127.0.0.1:8000/api/v1/logout',
+    {},
+    {
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+      }
+    }
+  ).then(() => {
+    localStorage.removeItem('token')
+    toast.success('See, ya!')
+    router.replace('/login')
+  }).catch((err) => {
+    console.log(err)
+    toast.error('Logout failed, please try again!')
+  })
+}
 </script>
 
 <template>
   <aside
     class="bg-neutral-900 border-r border-neutral-800 p-5 h-screen w-[240px] sticky top-0 bottom-0 left-0 overflow-y-auto z-[20000]">
-    <div class="flex py-2 items-center justify-between mt-5 cursor-pointer hover:bg-[#1d1d1d] rounded-full transition-all" @click="() => { isProjectsOpen = !isProjectsOpen }">
+    <div
+      class="flex py-2 items-center justify-between mt-5 cursor-pointer hover:bg-[#1d1d1d] rounded-full transition-all"
+      @click="() => { isProjectsOpen = !isProjectsOpen }">
       <div class="flex gap-3 items-center">
         <IconBriefcase2 class="w-10" />
         Projects
@@ -35,15 +59,16 @@ const isProjectsOpen = ref(true)
     </RouterLink>
 
 
-    <RouterLink to="/on-progress" class="flex py-2 gap-3 mt-5 hover:bg-[#1d1d1d] rounded-full transition-all">
+    <div @click="handleLogout"
+      class="flex py-2 gap-3 mt-5 hover:bg-[#1d1d1d] rounded-full transition-all cursor-pointer">
       <IconLogout class="w-10" />
       Log out
-    </RouterLink>
+    </div>
   </aside>
 </template>
 
 <style scoped>
-.router-link-exact-active{
+.router-link-exact-active {
   @apply bg-[#1d1d1d] rounded-full border border-neutral-700;
 }
 </style>
